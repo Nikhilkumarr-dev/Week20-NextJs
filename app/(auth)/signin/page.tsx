b6d174d72d6ceab63d0f16366f3fd1290d8c8374
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2025-01-21 14:51:55 +0530

    dynamic routing

--- app/(auth)/signin/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Signin(){
-    return <div>
-        <input type="text" placeholder="name" ></input>
-        <input type="text" placeholder="password"></input>
-        <button>Signin</button>
-    </div>
-}
--- app/(auth)/signup/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Signup(){
-    return <div>
-        <input type="text" placeholder="name" ></input>
-        <input type="text" placeholder="password"></input>
-        <button>Signup</button>
-    </div>
-}
