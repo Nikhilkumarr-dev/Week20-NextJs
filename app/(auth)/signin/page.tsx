7ec6d775be81f9dbfb5c324b34dca8b6cdbe462d
Nikhilkumarr-dev <nikhilkumarkarnakota@gmail.com>
2025-01-21 14:24:05 +0530

    RouteGroups in Nextjs

---
+++ app/(auth)/signin/page.tsx
@@ -0,0 +1,7 @@
+export default function Signin(){
+    return <div>
+        <input type="text" placeholder="name" ></input>
+        <input type="text" placeholder="password"></input>
+        <button>Signin</button>
+    </div>
+}
---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,7 @@
+export default function Signup(){
+    return <div>
+        <input type="text" placeholder="name" ></input>
+        <input type="text" placeholder="password"></input>
+        <button>Signup</button>
+    </div>
+}
